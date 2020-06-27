7e76fede6cc529c98071270387dd41445345fb43
1398abhisingh911 <52207042+1398abhisingh911@users.noreply.github.com>
2020-06-27 07:40:32 +0530

    f

--- testing.sh
+++ testing.sh
@@ -1,5 +1,5 @@
 nodeport=$(kubectl get svc -o jsonpath={.items[*].spec.ports[*].nodePort})
-status=$(curl -s -w "%{http_code}" -o /dev/nell http://192.168.99.100:$nodeport)
+status=$(curl -s -w "%{http_code}" -o /dev/null http://192.168.99.117:$nodeport)
 
 if [ $status -eq 200 ]
 then
