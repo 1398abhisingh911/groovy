eac32b1f8419457f0a009ef7a49e599d2166c3d9
1398abhisingh911 <52207042+1398abhisingh911@users.noreply.github.com>
2020-06-27 07:35:52 +0530

    a

--- create_jobs.groovy
+++ create_jobs.groovy
@@ -2,9 +2,7 @@ job("1 Git Pull") {
   description("Pulls Code From GitHub Repository")  
   
   scm {
-    github("1398abhisingh911
-/
-groovy", "master") 
+    github("1398abhisingh911/tasksec", "master") 
   }
 
   triggers {
